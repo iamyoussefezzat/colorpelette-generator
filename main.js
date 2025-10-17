f523b323b20c2f6151d643ea735c44a94472d959
Youssef M. Ezzat <yz22102004@gmail.com>
2025-10-17 23:25:32 +0300

    feat: add color palette generator with copy functionality

---
+++ main.js
@@ -0,0 +1,65 @@
+const generateBtn = document.getElementById('Generator-btn');
+const paletteContainer = document.querySelector('.palette-container');
+
+
+
+generateBtn.addEventListener('click', generatePalette);
+
+paletteContainer.addEventListener('click', function(e) {
+    if (e.target.classList.contains("copy-btn")) {
+
+      const hexValue = e.target.previousElementSibling.textContent;
+
+       navigator.clipboard.writeText(hexValue)
+       .then(() => showCopySucess(e.target))
+         .catch(() => alert('Failed to copy the color code!'));
+    }
+    else if(e.target.classList.contains("color")) {
+        const hexValue = e.target.nextElementSibling.querySelector('.hex-value').textContent;
+        navigator.clipboard.writeText(hexValue)
+        .then(() => showCopySucess(e.target.nextElementSibling.querySelector('.copy-btn')))
+          .catch(() => alert('Failed to copy the color code!'));
+    }
+});
+function showCopySucess(element) {
+    element.classList.remove("far","fa-copy");
+    element.classList.add("fas","fa-check");
+
+    element.style.color = "green";
+
+    setTimeout(() => {
+        element.classList.remove("fas","fa-check");
+        element.classList.add("far","fa-copy"); 
+        element.style.color = "";       
+    }, 1500);
+}
+function generatePalette() {
+        const colors = [];
+        for (let i = 0; i < 5; i++) {
+            colors.push(generatRandomColor());
+        }
+   updatepalette(colors);     
+}
+
+function generatRandomColor() {
+    const letters = '0123456789ABCDEF'; // Hexadecimal characters 
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function updatepalette(colors) {
+   const colorBoxes = document.querySelectorAll('.color-box');
+
+   colorBoxes.forEach((box, index) => {
+        const color = colors[index];
+        const colorDiv = box.querySelector('.color');
+        const HexValue = box.querySelector('.hex-value');
+
+      colorDiv.style.backgroundColor = color;  
+        HexValue.textContent = color;
+   });
+}
+
+generatePalette();
